50fd5d5334cee1be859cdbb3d79e0e13df5bc27f
Roman <4464530+roman8128@users.noreply.github.com>
2025-01-24 12:54:40 +0100

    Added configuration

--- src/main/java/com/demo/demo/DemoApplication.java
+++ src/main/java/com/demo/demo/DemoApplication.java
@@ -1,13 +1,28 @@
 package com.demo.demo;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.java.Log;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+@Log
+@AllArgsConstructor
+public class DemoApplication implements CommandLineRunner {
+	private PizzaConfig pizzaConfig;
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
+    @Override
+    public void run(String... args) throws Exception {
+        log.info(
+                String.format("I want a %s crusp pizza with a %s sauce and %s topping",
+                        pizzaConfig.getCrust(),
+                        pizzaConfig.getSauce(),
+                        pizzaConfig.getTopping())
+        );
+    }
 }
